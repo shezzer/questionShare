f35174b8736d16c4dfa82fb7950d1dfd543f7209
shezzer <shezzer_he@163.com>
2018-01-05 10:58:16 +0800

    FeedHandler简单补充，功能还未完善

--- src/main/java/com/project/async/handler/FeedHandler.java
+++ src/main/java/com/project/async/handler/FeedHandler.java
@@ -1,20 +1,19 @@
 package com.project.async.handler;
 
+import com.alibaba.fastjson.JSONObject;
 import com.project.async.EventHandler;
 import com.project.async.EventModel;
 import com.project.async.EventType;
-import com.project.model.EntityType;
-import com.project.model.Feed;
-import com.project.model.Message;
+import com.project.model.*;
+import com.project.service.FeedService;
 import com.project.service.MessageService;
+import com.project.service.QuestionService;
 import com.project.service.UserService;
 import com.project.util.WendaUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Sherl on 2017/12/27.
@@ -22,27 +21,52 @@ import java.util.List;
 @Component
 public class FeedHandler implements EventHandler{
     @Autowired
-    MessageService messageService;
+    FeedService feedService;
     @Autowired
     UserService userService;
+    @Autowired
+    QuestionService questionService;
+
+    private String buildFeedData(EventModel model) {
+        Map<String, String> map = new HashMap<String ,String>();
+        // 触发用户是通用的
+        User actor = userService.getUser(model.getActorId());
+        if (actor == null) {
+            return null;
+        }
+        map.put("userId", String.valueOf(actor.getId()));
+        map.put("userHead", actor.getHeadUrl());
+        map.put("userName", actor.getName());
+
+        if (model.getType() == EventType.COMMENT ||
+                (model.getType() == EventType.FOLLOW  && model.getEntityType() == EntityType.ENTITY_QUESTION)) {
+            Question question = questionService.findById(model.getEntityId());
+            if (question == null) {
+                return null;
+            }
+            map.put("questionId", String.valueOf(question.getId()));
+            map.put("questionTitle", question.getTitle());
+            return JSONObject.toJSONString(map);
+        }
+        return null;
+    }
 
     @Override
     public void doHandle(EventModel model) {
         Feed feed = new Feed();
-        message.setFromId(WendaUtil.SYSTEM_USERID);
-        message.setToId(model.getEntityOwnerId());
-        message.setCreatedDate(new Date());
-        if (model.getEntityType() == EntityType.ENTITY_QUESTION)
-            message.setContent("用户"+userService.getUser(model.getActorId()).getName()
-                +"关注了你的问题，http://localhost:8080/question/"+model.getEntityId());
-        else if(model.getEntityType() == EntityType.ENTITY_USER)
-            message.setContent("用户"+userService.getUser(model.getActorId()).getName()
-                    +"关注了你，http://localhost:8080/user/"+model.getActorId());
-        messageService.addMessage(message);
+        feed.setCreatedDate(new Date());
+        feed.setType(model.getType().getValue());
+        feed.setUserId(model.getActorId());
+        feed.setData(buildFeedData(model));
+        if (feed.getData() == null) {
+            // 不支持的feed
+            return;
+        }
+        feedService.addFeed(feed);
     }
 
     @Override
     public List<EventType> getSupportEventTypes() {
-        return Arrays.asList(EventType.FOLLOW);
+        return Arrays.asList(new EventType[]{EventType.COMMENT, EventType.FOLLOW});
     }
 }
